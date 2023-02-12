83e1913ca01345a4c078c0ed224f8895ef108afe
AJBrinas <drewbrinas@gmail.com>
2023-02-12 22:01:55 +0800

    Design
    Almost done in Homepage

---
+++ js/nav.js
@@ -0,0 +1,26 @@
+const createNav = () => {
+    let nav = document.querySelector('.navbar');
+
+    nav.innerHTML = `
+        <div class="nav">
+            <img src="img/logo.png" class="brand-logo" alt="">
+            <div class="nav-items">
+                <div class="search">
+                    <input type="text" class="search-box" placeholder="search brand, product">
+                    <button class="search-btn">search</button>
+                </div>
+                <a href="#"><img src="img/user.png" alt=""></a>
+                <a href="#"><img src="img/cart.png" alt=""></a>
+            </div>
+        </div>
+        <ul class="links-container">
+            <li class="link-item"><a href="#" class="link">home</a></li>
+            <li class="link-item"><a href="#" class="link">Products</a></li>
+            <li class="link-item"><a href="#" class="link">About Us</a></li>
+            <li class="link-item"><a href="#" class="link">Size Chart</a></li>
+            <li class="link-item"><a href="#" class="link">FAQs</a></li>
+        </ul>
+    `;
+}
+
+createNav();
