f6f76c478d2ca12fd83a6a926911b01baa083819
Janepajela <ms.janesamerpajela@gmail.com>
2023-02-15 23:58:15 +0800

    updating draft

--- admin/category.php
+++ admin/category.php
@@ -2,23 +2,56 @@
     include 'sidebar.php';
 ?>
 
+
 <main>
-			<div class="head-title">
-				<div class="left">
-					<h1>Dashboard</h1>
-					<ul class="breadcrumb">
-						<li>
-							<a href="#">Dashboard</a>
-						</li>
-						<li><i class='bx bx-chevron-right' ></i></li>
-						<li>
-							<a class="active" href="#">Home</a>
-						</li>
-					</ul>
+<link rel="stylesheet" href="admin/css/category.css">
+			<div class="CategoryContainer">
+				<div class="Category">
+					<h1>Category</h1>
 				</div>
 			</div>
-</main>
+			<a class="add-new-category" href="add_category.php">Add New Category</a>
 
+			<div class="table-data">
+				<div class="All Categories">
+					<div class="head">
+						<h3>Orders</h3>
+						<i class='bx bx-search' ></i>
+						<i class='bx bx-filter' ></i>
+						
+						
+					</div>
+					<table>
+						<thead>
+							<tr>
+								<th>All Categories</th>
+								<th>Actions</th>
+							</tr>
+						</thead>
+
+						<tbody>
+							<tr>
+								<td>
+									<p>T-Shirts</p>
+								</td>
+								<td>
+									<i class='bx bx-trash'></i>
+								</td>
+							</tr>
+							<tr>
+								<td>
+									<p>Lanyards</p>
+								</td>
+								<td>
+									<i class='bx bx-trash'></i>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</main>
 <script src="js/script.js"></script>
+
+
 </body>
 </html>
--- admin/products.php
+++ admin/products.php
@@ -1,3 +1,84 @@
 <?php
     include 'sidebar.php'
 ?>
+
+<main>
+			<div class="head-title">
+				<div class="left">
+					<h1>Products</h1>
+					<ul class="breadcrumb">
+						<li>
+							<a href="#">Products</a>
+						</li>
+						<li><i class='bx bx-chevron-right' ></i></li>
+						<li>
+							<a class="active" href="#">Home</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+
+
+			<div class="table-data">
+				<div class="order">
+					<div class="head">
+						<h3>Orders</h3>
+						<i class='bx bx-search' ></i>
+						<i class='bx bx-filter' ></i>
+						
+					</div>
+					<table>
+						<thead>
+							<tr>
+								<th>User</th>
+								<th>Date Order</th>
+								<th>Status</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>
+									<img src="img/people.png">
+									<p>Royette Vergara</p>
+								</td>
+								<td>01-10-2021</td>
+								<td><span class="status completed">Completed</span></td>
+							</tr>
+							<tr>
+								<td>
+									<img src="img/people.png">
+									<p>Janna Manuela</p>
+								</td>
+								<td>01-10-2021</td>
+								<td><span class="status pending">Pending</span></td>
+							</tr>
+							<tr>
+								<td>
+									<img src="img/people.png">
+									<p>Mejela Gojol</p>
+								</td>
+								<td>01-10-2021</td>
+								<td><span class="status process">Process</span></td>
+							</tr>
+							<tr>
+
+								
+								<!-- <td>
+									<img src="img/people.png">
+									<p>Janna Manuela</p>
+								</td>
+								<td>01-10-2021</td>
+								<td><span class="status pending">Pending</span></td>
+							</tr>
+							<tr>
+								<td>
+									<img src="img/people.png">
+									<p>Janna Manuela</p>
+								</td>
+								<td>01-10-2021</td>
+								<td><span class="status completed">Completed</span></td>
+							</tr>
+							 -->
+						</tbody>
+					</table>
+				</div>
