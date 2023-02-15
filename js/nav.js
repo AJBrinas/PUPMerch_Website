cd26dcfafd02eb7f24c06c37f905abfcdd173d10
Jaja <jmaenriquez16@gmail.com>
2023-02-15 14:14:09 +0800

    search button

--- js/nav.js
+++ js/nav.js
@@ -17,7 +17,9 @@ const createNav = () => {
             <div class="nav-items">
                 <div class="search">
                     <input type="text" class="search-box" placeholder="search brand, product">
-                    <button class="search-btn">search</button>
+                    <button class="search-btn">
+                    <img src="img/search.png" alt="">
+                    </button>
                 </div>
                 <a href="#"><img src="img/user.png" alt=""></a>
                 <a href="#"><img src="img/cart.png" alt=""></a>
