d02dca27b90d7e44a6c67c6438a86533f5cd0343
Janepajela <ms.janesamerpajela@gmail.com>
2023-02-16 00:53:08 +0800

    changing eme

--- admin/category.php
+++ admin/category.php
@@ -4,52 +4,60 @@
 
 
 <main>
-<link rel="stylesheet" href="admin/css/category.css">
-			<div class="CategoryContainer">
-				<div class="Category">
-					<h1>Category</h1>
+			<div class="head-title">
+				<div class="left">
+					<h1>Categories</h1>
+					<ul class="breadcrumb">
+						<li>
+							<a href="#">Dashboard</a>
+						</li>
+						<li><i class='bx bx-chevron-right' ></i></li>
+						<li>
+							<a class="active" href="#">Categories</a>
+						</li>
+					</ul>
 				</div>
 			</div>
-			<a class="add-new-category" href="add_category.php">Add New Category</a>
+
 
 			<div class="table-data">
-				<div class="All Categories">
+				<div class="order">
 					<div class="head">
-						<h3>Orders</h3>
-						<i class='bx bx-search' ></i>
+						<h3>All Categories</h3>
+						<i class='bx bxs-add-to-queue' ></i><p>Add Category </p>
 						<i class='bx bx-filter' ></i>
-						
 						
 					</div>
 					<table>
 						<thead>
 							<tr>
-								<th>All Categories</th>
+								<th>Title</th>
+								<th></th>
 								<th>Actions</th>
 							</tr>
 						</thead>
-
 						<tbody>
 							<tr>
 								<td>
-									<p>T-Shirts</p>
+								    <i class='bx bx-store-alt'></i>
+									<p>T-shirts</p>
 								</td>
-								<td>
-									<i class='bx bx-trash'></i>
-								</td>
+								<td></td>
+								<!--<td><span class="status process">Edit</span><span class="status completed">Delete</span></td>-->
+								<td><i class='bx bxs-edit' ></i><i class='bx bx-trash' ></i></td>
 							</tr>
 							<tr>
 								<td>
+								    <i class='bx bx-store-alt'></i>
 									<p>Lanyards</p>
 								</td>
-								<td>
-									<i class='bx bx-trash'></i>
-								</td>
+								<td></td>
+								<td><i class='bx bxs-edit' ></i><i class='bx bx-trash' ></i></td>
 							</tr>
+							
 						</tbody>
 					</table>
 				</div>
-			</main>
 <script src="js/script.js"></script>
 
 
