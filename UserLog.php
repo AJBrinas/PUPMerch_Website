9d58feaa3ee4783a5a60c2d21654065d61d8f500
RoyetteVergara25 <royettehvergara@iskolarngbayan.pup.edu.ph>
2023-02-15 13:01:45 -1200

    register.php
    image, php file , css file

---
+++ UserLog.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Customer Log In</title>
+    <link rel="stylesheet" href="users.css">
+  </head>
+  <body>
+
+    <link rel="stylesheet" href="users.css">
+    <script src="img/slideshow.js" defer></script>
+        <header class="intro">
+            <div class="intro-slideshow">
+        <img src="img/1.png" class="slideshow-img">
+        <img src="img/2.png" class="slideshow-img">
+        <img src="img/3.png" class="slideshow-img">
+        <img src="img/4 .png" class="slideshow-img">
+            </div>
+        </header>
+
+
+    <div class="center">
+      <h1>User Login</h1>
+      <form method="post">
+        <div class="txt_field">
+          <input type="text" required>
+          <span></span>
+          <label>Username</label>
+        </div>
+        <div class="txt_field">
+          <input type="password" required>
+          <span></span>
+          <label>Password</label>
+        </div>
+        <div class="pass"></div>
+
+        <input type="submit" value="Login">
+
+
+        <div class="signup_link">
+          Not a member? <a href="Register.html">Signup</a>
+        </div>
+      </form>
+    </div>
+
+  </body>
+</html>
+
