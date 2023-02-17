c5e0953cb6174373602b7a55deb9c605f8bfb31c
RoyetteVergara25 <royettehvergara@iskolarngbayan.pup.edu.ph>
2023-02-16 16:32:31 -1200

    ProductList and Users
    List of Products and Users

---
+++ admin/Users.php
@@ -0,0 +1,106 @@
+
+<?php 
+include 'sidebar.php'
+?>
+
+<main>
+			<div class="head-title">
+				<div class="left">
+					<h1>ALL USERS</h1>
+					<ul class="breadcrumb">
+						<li>
+							<a href="#">Dashboard</a>
+						</li>
+						<li><i class='bx bx-chevron-right' ></i></li>
+						<li>
+							<a class="active" href="#">Users</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+
+
+			<div class="table-data">
+				<div class="order">
+					<div class="head">
+						<h3>USERS</h3>
+						<i class='bx bx-search' ></i>
+						<i class='bx bx-filter' ></i>
+						
+					</div>
+					<table>
+						<thead>
+							<tr>
+                             <th>User ID</th>
+								<th></th>
+								<th>Full Name</th>
+								<th></th>
+								<th>Email</th>
+								<th></th>
+								<th>Contact No.</th>
+								<th></th>
+								<th>Actions</th>
+							</tr>
+						</thead>
+						<tbody>
+						<tbody>
+							<tr>
+								<td>
+								    
+									<p>01</p>
+								</td>
+								<td></td>
+								<td>
+								    <p>Royette Vergara</p>
+									
+								</td>
+								<td></td>
+								<td>
+								
+									<p>rv@gmail.com</p>
+									
+								</td>
+								<td></td>
+								<td>
+								
+									<p>0912345678</p>
+									
+								</td>
+								<td></td>
+
+                               
+
+								<!--<td><span class="status process">Edit</span><span class="status completed">Delete</span></td>-->
+								<td><i class='bx bxs-edit' ></i><i class='bx bx-trash' ></i></td>
+							
+
+								<tr>
+								<td>
+								    
+									<p>02</p>
+								</td>
+								<td></td>
+								<td>
+								   <p>Jane Pajela</p>
+									
+								</td>
+								<td></td>
+								<td>
+								
+									<p>Jsp@gmail.com</p>
+									
+								</td>
+								<td></td>
+								<td>
+								
+									<p>0987654321</p>
+									
+								</td>
+								<td></td>
+								<!--<td><span class="status process">Edit</span><span class="status completed">Delete</span></td>-->
+								<td><i class='bx bxs-edit' ></i><i class='bx bx-trash' ></i></td>
+							
+							
+						</tbody>
+					</table>
+				</div>
