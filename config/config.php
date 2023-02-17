3f55fb6dffad1e534c912a5d99cec02c92cee57b
AJBrinas <drewbrinas@gmail.com>
2023-02-18 02:01:56 +0800

    changes/deletions
    deleted: product.php
    renamed:  Productlist.php -> products
    
    config:not done yet

--- config/config.php
+++ config/config.php
@@ -1,16 +1,28 @@
 <?php 
 
+class Database {
     // Configuration 
-    $host = "ANDREW\SQLEXPRESS"; // Server Name
-    $user = "";
-    $pass = "";
-    $db_name = "merch_db"; // Database Name
 
-    try{
-        $connect = new PDO("sqlsrv:Server=$host;Database=$db_name", $user, $pass); // PDO dll Required
-        $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo 'Connected';
-    } catch (PDOException $e){
-        die("Unsuccessful Connection: ".$e->getMessage());
+    private $db_host = "ANDREW\SQLEXPRESS"; // Server Name
+    private $user = "";
+    private $pass = "";
+    private $db_name = "merch_db"; // Database Name
+
+    public function conn()
+    {
+        try{
+            $connect = new PDO("sqlsrv:Server=$this->db_host;Database=$this->db_name", $this->user, $this->pass); // PDO dll Required
+            $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return;
+        } catch (PDOException $e){
+            die("Unsuccessful Connection: ".$e->getMessage());
+        }
+    
     }
+
+}
+
+$this->conn();
 ?>
+
+
