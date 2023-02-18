15cfcb1c1904e3263f2f433d238b8c8f00fcc3fd
AJBrinas <drewbrinas@gmail.com>
2023-02-18 22:47:35 +0800

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,4 +1,6 @@
-<?php 
+<?php
+
+use LDAP\Result;
 
 class Database {
     // Configuration 
@@ -8,21 +10,48 @@ class Database {
     private $pass = "";
     private $db_name = "merch_db"; // Database Name
 
-    public function conn()
+    public function db_conn()
     {
         try{
             $connect = new PDO("sqlsrv:Server=$this->db_host;Database=$this->db_name", $this->user, $this->pass); // PDO dll Required
             $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            return;
-        } catch (PDOException $e){
+            return true;
+        } 
+        catch (PDOException $e){
             die("Unsuccessful Connection: ".$e->getMessage());
+            return false;
         }
-    
+    }
+
+    public function read($query)
+    {
+        $conn = $this->db_conn();
+        $result = mysqli_query($conn, $query);
+        
+        if(!$result)
+        {
+            return false;
+        }
+        
     }
 
 }
 
-$this->conn();
+$DB = new Database();
+
+$query = "SELECT * FROM categories";
+$data = $DB->read($query);
+//-----------------------------------------------------------------------
+    // $db_host = "ANDREW\SQLEXPRESS"; // Server Name
+    // $user = "";
+    // $pass = "";
+    // $db_name = "merch_db"; // Database Name
+
+    // try{
+    //     $connect = new PDO("sqlsrv:Server=$db_host;Database=$db_name", $user, $pass); // PDO dll Required
+    //     $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //     echo 'Fuck this';
+    // } catch (PDOException $e){
+    //     die("Unsuccessful Connection: ".$e->getMessage());
+    // }
 ?>
-
-
