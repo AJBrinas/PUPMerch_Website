88a80aac412fa24fd0b6c5061365b528014d51bc
Janepajela <ms.janesamerpajela@gmail.com>
2023-02-18 11:01:41 +0800

    changing about us and fqs
    all goods in the hoods

--- faqs.php
+++ faqs.php
@@ -15,7 +15,7 @@
             <p class= "Q1A"> A: We are using a 80% cotton 20% polyester blend for our t-shirts to ensure that it doesn't wear out easily. (Unless stated otherwise). </p>
             <div class="Q1-Image">
                 <a target="_blank" href="img/Q1.png">
-                <img src="img/Q1.png" alt="Q1" width="1300" height="1300">
+                <img src="img/Q1.png" alt="Q1" width="800" height="800">
                 </a>
             </div>
         <!--FAQ 2-->
@@ -23,7 +23,7 @@
             <p class= "Q2A"> A: All the products which are shown on our site are available. Order lead time depends on the products and quantities. </p>
             <div class="Q2-Image">
                 <a target="_blank" href="img/Q2.png">
-                <img src="img/Q2.png" alt="Q2" width="1300" height="1300">
+                <img src="img/Q2.png" alt="Q2" width="800" height="800">
                 </a>
             </div>
         <!--FAQ 3-->
@@ -31,7 +31,7 @@
             <p class= "Q3A"> A: We accept Credit Card, Debit Card, GCash and Paypal. </p>
             <div class="Q3-Image">
                 <a target="_blank" href="img/Q3.png">
-                <img src="img/Q3.png" alt="Q3" width="1300" height="1300">
+                <img src="img/Q3.png" alt="Q3" width="800" height="800">
                 </a>
             </div>
         <!--FAQ 4-->
@@ -39,7 +39,7 @@
             <p class= "Q4A"> A: Made to order shirts will add an additional 3-7 days on the processing time. </p>
             <div class="Q4-Image">
                 <a target="_blank" href="img/Q4.png">
-                <img src="img/Q4.png" alt="Q4" width="1300" height="1300">
+                <img src="img/Q4.png" alt="Q4" width="800" height="800">
                 </a>
             </div>
         <!--FAQ 5-->
@@ -47,7 +47,7 @@
             <p class= "Q5A"> A: You can get your order within 3-7 business days, depending on your location. </p>
             <div class="Q5-Image">
                 <a target="_blank" href="img/Q5.png">
-                <img src="img/Q5.png" alt="Q5" width="1300" height="1300">
+                <img src="img/Q5.png" alt="Q5" width="800" height="800">
                 </a>
             </div>
         <!--FAQ 6-->
@@ -55,7 +55,7 @@
             <p class= "Q6A"> A: You can order easily using our online platform. When you find a product you need, you can add it to cart, login and go through the ordering process. After the order is ready, you will receive order summary to your email. Order summary will also be stored to your account. </p>
             <div class="Q6-Image">
                 <a target="_blank" href="img/Q6.png">
-                <img src="img/Q6.png" alt="Q6" width="1300" height="1300">
+                <img src="img/Q6.png" alt="Q6" width="800" height="800">
                 </a>
             </div>
         <!--FAQ 7-->
@@ -63,7 +63,7 @@
             <p class= "Q7A"> A: Speeding up the process. By ordering online you will you will get prices faster and you will be able to go through order confirmation and payment process much faster. This could save days of your time. </p>
             <div class="Q7-Image">
                 <a target="_blank" href="img/Q7.png">
-                <img src="img/Q7.png" alt="Q7" width="1300" height="1300">
+                <img src="img/Q7.png" alt="Q7" width="800" height="800">
                 </a>
             </div>
         <!--FAQ 8-->
@@ -71,7 +71,7 @@
             <p class= "Q8A"> A: For any concerns, you can reach us out by sending us a message on likhaclothing@gmail.com </p>
             <div class="Q8-Image">
                 <a target="_blank" href="img/Q8.png">
-                <img src="img/Q8.png" alt="Q8" width="1300" height="1300">
+                <img src="img/Q8.png" alt="Q8" width="800" height="800">
                 </a>
             </div>
     <!--FAQ 9-->
@@ -79,7 +79,7 @@
             <p class= "Q9A"> A: Yes, you can return a product. Please read our terms and condition and send us a message on likhaclothing@gmail.com </p>
             <div class="Q9-Image">
                 <a target="_blank" href="img/Q9.png">
-                <img src="img/Q9.png" alt="Q9" width="1300" height="1300">
+                <img src="img/Q9.png" alt="Q9" width="800" height="800">
                 </a>
             </div>
         <!--FAQ 10-->
@@ -87,7 +87,7 @@
             <p class= "Q10A"> A: Wash inside-out with lukewarm water, It is best not to use fabric softeners on the shirts. If there are wrinkles on the print. Do not iron directly on the print, instead iron inside-out. Hang-dry works best for drying the clothes </p>
             <div class="Q10-Image">
                 <a target="_blank" href="img/Q10.png">
-                <img src="img/Q10.png" alt="Q10" width="1300" height="1300">
+                <img src="img/Q10.png" alt="Q10" width="800" height="800">
                 </a>
             </div>
 
