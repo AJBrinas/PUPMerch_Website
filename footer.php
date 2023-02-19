fc253e8e912349f598b6eca80ff9b52934621490
Jaja <jmaenriquez16@gmail.com>
2023-02-19 13:17:19 +0800

    new footer

--- footer.php
+++ footer.php
@@ -1,19 +1,80 @@
+<!DOCTYPE html>
+  <head>
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Heebo:wght@300;400;500;600;700;800;900&display=swap"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
+    <link rel="stylesheet" href="css/footer.css" />
+  </head>
+  <body>
+
 <footer>
-        <div class="footer-content">
-            <img src="img/footer-logo.png" class="logo" alt="">
-            <!-- <div class="footer-ul-container">
-                <ul class="category">
-                    <li class="category-title">Contact us</li>
-                    <li><a href="#" class="footer-link">Email</a></li>
-                    <li><a href="#" class="footer-link">Facebook Page</a></li>
-                    <li><a href="#" class="footer-link">Instagram</a></li> -->
-                <!-- </ul>
-                <div class="footer-social-container">
-                    <div>
-                        <a href="#" class="social-link">terms & services</a>
-                        <a href="#" class="social-link">privacy page</a>
-                    </div>
-            </div> -->
+<div class="container">
+     <div class="wrapper">
+    <div class="footer-widget">
+        <img src="img/footer-logo.png" class="logo" >
+        <p class="desc">
+            The fashion-forward apparel line for every PUPian, because excellent brains deserve outstanding look. We strive to create one-of-a-kind t-shirt designs that can be worn on a regular basis.
+            </p>
+            <ul class="socials">
+              <li>
+                <a href="#">
+                  <i class="fab fa-facebook-f"></i>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <i class="fab fa-twitter"></i>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <i class="fab fa-instagram"></i>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <i class="fab fa-linkedin-in"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div class="footer-widget">
+            <h6>Quick Link</h6>
+            <ul class="links">
+              <li><a href="#">Home</a></li>
+              <li><a href="#">Products</a></li>
+              <li><a href="#">About us</a></li>
+              <li><a href="#">Size Chart</a></li>
+              <li><a href="#">FAQS</a></li>
+            </ul>
+          </div>
+          <div class="footer-widget">
+            <h6>Products</h6>
+            <ul class="links">
+              <li><a href="#">T-Shirts</a></li>
+              <li><a href="#">Lanyards</a></li>
+              <li><a href="#">Jacket</a></li>
+              <li><a href="#">Notebook</a></li>
+            </ul>
+          </div>
+          <div class="footer-widget">
+            <h6>Help &amp; Support</h6>
+            <ul class="links">
+              <li><a href="#">Support Center</a></li>
+              <li><a href="#">Feedback</a></li>
+              <li><a href="#">Contact</a></li>
+              <li><a href="#">terms &amp; conditions</a></li>
+            </ul>
+          </div>
         </div>
+        <div class="copyright-wrapper">
+          <p>
+            ALL RIGHTS RESERVED BY
+            <a href="#" target="blank">LIKHA CLOTHING</a>
+          </p>
+        </div>
+      </div>
     </footer>
-    
+  </body>
+
+   
