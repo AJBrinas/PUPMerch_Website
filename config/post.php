108d43340a6a3cd694b319dcab286d6f04a6c446
codelala <93863020+Axyszce@users.noreply.github.com>
2023-02-21 22:25:02 +0800

    post

--- config/post.php
+++ config/post.php
@@ -1,6 +1,20 @@
 <?php 
-    include_once 'config.php'
+include "config.php";
 
+if(isset($_POST['p_cat'])){
+    $cat = $_POST['p_cat'];
+    $database = new Database;
+	$database->select('categories', '*');
+    $result = $database->getResults();
+
+    $output = [];
+    if($result>0){
+        $output['$result'] = $result; 
+    }
+}
+
+if(isset($_POST['create'])){
     
+}
 
 ?>
