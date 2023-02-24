55715c07f53e7721bc9b2e29c78d7d66c47fb897
codelala <93863020+Axyszce@users.noreply.github.com>
2023-02-25 00:13:44 +0800

    paayos

--- admin/addproduct.php
+++ admin/addproduct.php
@@ -42,23 +42,26 @@
 
 include 'sidebar.php' 
 ?>
-
-        <link rel="stylesheet" href="css/addprod.css">
-        <link rel="stylesheet" href="css/bootstrap.min.css">
-<body>
-    <div class="addprod">
-        
-    <div class="admin-content-container">
-        <h2 class="admin-heading">Add New Product</h2>
-        <form id="createProduct" class="add-post-form row" action = "../config/post.php" method="post" enctype="multipart/form-data">
-            
-                <div class="form-group category">
-                    <label for="">Product Category</label>
+<html>
+    <body>
+        <head>
+        <link rel="stylesheet" href="css/addproducts.css">
+					<h1>Add New Products</h1>
+        </head>
+    
+            <div>
+                <form id="createProduct" class="add-post-form row" action = "../config/post.php" method="post" enctype="multipart/form-data">
+                <div>
+                    <label>Product Title</label>
+                    <input type="text"  name="product_title" id="product_title" placeholder="Ex: Isko Shirt..." requried/>
+                </div>
+                <div>
+                    <label>Product Category</label>
                     <?php 
 					$database = new Database;
 					$database->select('categories', '*');
 					$result = $database->getResults();
-				?>
+				    ?>
                     <select class="options" name="product_cat" id="product_cat">
                     <option value="" selected disabled>Select Category</option>
                     <?php 
@@ -78,59 +81,38 @@ include 'sidebar.php'
                         </option>
 						</tr>
 						<?php }?>
-                    
                     </select>
                 </div>
-            
-                <div class="col-md-7">
-                <div class="form-group">
-                    <label for="">Product Title</label>
-                    <input type="text" class="form-control product_title" name="product_title" id="product_title" placeholder="Product Title" requried/>
-                </div>
-
-                <div class="form-group">
-                    <label for="">Product Price</label>
+                <div>
+                    <label>Product Price</label>
                     <input type="text" class="form-control product_price" name="product_price" requried value="">
                 </div>
-
-                <div class="form-group">
-                    <label for="">Product Description</label>
-                    <textarea class="form-control product_description" name="product_desc" id="product_desc" rows="8" cols="80" requried></textarea>
+                <div>
+                    <label>Available Quantity</label>
+                    <input type="number" class="form-control product_qty" name="product_qty" id="product_qty" requried>
                 </div>
-                <div class="show-error"></div>
-            </div>
-				
-                <div class="">
-                    <label for="">Featured Image</label>
-                    <input type="file" name="featured_img" id="product_image" required>  
-                    <img id="file" src="" width="150px"/>
+                <div>
+                    <label>Product Description</label>
+                    <textarea name="product_desc" id="product_desc" rows="8" cols="80" placeholder="Write something.." style="height:200px" requried></textarea>
                 </div>
-
-                <div class="form-group">
-                    <label for="">Available Quantity</label>
-                    <input type="number" class="form-control product_qty" name="product_qty" id="product_qty" requried value="">
+                <div>
+                    <label>Product Keywords</label>
+                    <input type="text" id="product_keywords" name="product_keywords"  required>
                 </div>
-                
-
-                <div class="form-group">
-                        <label>Product Keywords</label>
-                        <input type="text" id="product_keywords" name="product_keywords" required class="form-control" >
-                        </div>
-                    </div><
-
-                    <div class="form-group">
-                    <label>Status</label>
-                    <select class="form-control product_status" name="product_status" id="product_status>
-                        <option value="0">Publish</option>
-                        <option value="1">Draft</option>
-                    </select>
+                <div>
+                <label>Featured Image</label>
+                <input type="file" name="featured_img" id="product_image" required>  
+                    <!---<img id="file" src="" width="150px"/> --->
                 </div>
-
-                <div class="form-group">
-                    <input type="submit" class="btn-add-new" name="submit" id="submit" value="Submit">
+                <div>
+                <label>Status </label>
+                <select id="product_status" name="product_status">
+                    <option value="X" selected disabled>Select Status</option>
+                    <option selected value="1">Publish</option>
+                    <option value="0">Draft</option>
+                </select>
                 </div>
-            </div>
-        </form>
-    </div>
-    </div>
-</body>
+            </form>
+        </div>
+    </body>
+</html>
